5c52f7a3051fe95a2a3179dc71d8dbac001e3f73
klein-thibault <klein.thibault@gmail.com>
2019-01-05 17:44:59 -0500

    Moved everything to the root folder

---
+++ Sources/App/configure.swift
@@ -0,0 +1,38 @@
+import FluentPostgreSQL
+import Vapor
+
+/// Called before your application initializes.
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    /// Register providers first
+    try services.register(FluentPostgreSQLProvider())
+
+    /// Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    /// Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+
+    // Configure a PostgreSQL database
+    let config = PostgreSQLDatabaseConfig(hostname: "localhost",
+                                          port: 5432,
+                                          username: "postgres",
+                                          database: "postgres",
+                                          password: "vPYZxUmZLxYMNiRrkrVX",
+                                          transport: .cleartext)
+    let postgresql = PostgreSQLDatabase(config: config)
+
+    /// Register the configured PostgreSQL database to the database config.
+    var databases = DatabasesConfig()
+    databases.add(database: postgresql, as: .psql)
+    services.register(databases)
+
+    /// Configure migrations
+    var migrations = MigrationConfig()
+    services.register(migrations)
+
+}
