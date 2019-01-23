db62d01a7161fa198601445d4b795b97a7a8bbeb
klein-thibault <klein.thibault@gmail.com>
2019-01-23 00:32:12 -0500

    Updated code to connect to Neo database

--- Sources/App/Controllers/QAController.swift
+++ Sources/App/Controllers/QAController.swift
@@ -5,16 +5,13 @@ import Theo
 final class QAController {
 
     func testGraphDatabase(_ req: Request) throws -> Future<HTTPStatus> {
-//        let graphClient = try req.sharedContainer.make(GraphDatabaseClient.self)
-        let graphClient = try BoltClient(hostname: "localhost",
-                                         port: 7687,
-                                         username: "thibault",
-                                         password: "password",
-                                         encrypted: true)
+        let graphClient = try req.sharedContainer.make(GraphDatabaseClient.self)
+        graphClient.client.connectSync()
+
         let node = Node(label: "User", properties: ["email": "test@test.com"])
         let promise = req.eventLoop.newPromise(HTTPStatus.self)
 
-        graphClient.createNode(node: node) { (result) in
+        graphClient.client.createNode(node: node) { (result) in
             switch result {
             case let .failure(error):
                 promise.fail(error: error)
