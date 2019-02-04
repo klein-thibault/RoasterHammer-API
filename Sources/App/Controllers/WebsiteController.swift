d8ae1bbd83cd9cafa98c530793148fd9e1fce259
klein-thibault <klein.thibault@gmail.com>
2019-02-04 10:59:26 -0500

    Added styling and first feature of the home page listing the armies

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,6 +3,15 @@ import Leaf
 
 struct WebsiteController {
     func indexHandler(_ req: Request) throws -> Future<View> {
-        return try req.view().render("index")
+        let armyController = ArmyController()
+        return try armyController.armies(req).flatMap(to: View.self, { armies in
+            let context = IndexContext(title: "Homepage", armies: armies)
+            return try req.view().render("index", context)
+        })
     }
+}
+
+struct IndexContext: Encodable {
+    let title: String
+    let armies: [ArmyResponse]
 }
