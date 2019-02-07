015c541ef29ac1bcf68e9b3dd157a9fa028d79fa
klein-thibault <klein.thibault@gmail.com>
2019-02-07 17:52:48 -0500

    Working prototype for the add rules web UI

---
+++ Public/scripts/repeater-script.js
@@ -0,0 +1,15 @@
+$('.example').repeater({
+                       btnAddClass: 'r-btnAdd',
+                       btnRemoveClass: 'r-btnRemove',
+                       groupClass: 'r-group',
+                       minItems: 1,
+                       maxItems: 0,
+                       startingIndex: 0,
+                       showMinItemsOnLoad: false,
+                       reindexOnDelete: true,
+                       repeatMode: 'append',
+                       animation: null,
+                       animationSpeed: 400,
+                       animationEasing: 'swing',
+                       clearValues: true
+                       });
