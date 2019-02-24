3475d9e207ec930bb3f22d5c65c63227c3dd2539
klein-thibault <klein.thibault@gmail.com>
2019-02-24 16:11:22 -0500

    Updated package swift to add RoasterhammerShared as target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -14,15 +14,16 @@ let package = Package(
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
 
         // Web templating language framework package
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2")
 
         // Neo4j Database client
         // .package(url: "https://github.com/Neo4j-Swift/Neo4j-Swift.git", from: "4.0.2")
     ],
     targets: [
         //.target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Authentication", "Leaf", "Theo"]),
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Authentication", "Leaf"]),
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Authentication", "Leaf", "RoasterhammerShared"]),
         .target(name: "Run", dependencies: ["App"]),
+        .target(name: "RoasterhammerShared", path: "./RoasterhammerShared"),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
