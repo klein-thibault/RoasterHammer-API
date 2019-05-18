d3ede2b4bd98efbdbea0bf0393a8743e441cd1e2
klein-thibault <klein.thibault@gmail.com>
2019-05-17 22:20:52 -0400

    Updated code around keywords in web pages

--- Public/scripts/create-keywords.js
+++ Public/scripts/create-keywords.js
@@ -1,5 +1,6 @@
 $("#keywords").select2({
                        placeholder: "Enter Keywords",
                        tags: true,
+                       width: "100%",
                        tokenSeparators: [',']
                        });
